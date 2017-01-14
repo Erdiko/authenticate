5452e255c001c8772f697b460357cc2ff9e7d359
Andy Armstrong <andyarmstro@gmail.com>
2017-01-13 16:55:30 -0800

    created stub tests

---
+++ tests/phpunit/services/JWTAuthenticatorTest.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ *
+ * @todo add tests for the different template types
+ */
+namespace tests\phpunit;
+
+
+require_once dirname(__DIR__) . '/../ErdikoTestCase.php';
+
+
+class JWTAuthenticatorTest extends \tests\ErdikoTestCase
+{
+	public static function setUpBeforeClass()
+    {
+        @session_start();
+		$_SESSION = array();
+		ini_set("session.use_cookies",0);
+		ini_set("session.use_only_cookies",0);
+	}
+
+    protected function setUp()
+    {
+        @session_start();
+        //error_reporting(E_ALL);
+        //ini_set('display_errors', 1);
+    }
+
+}
