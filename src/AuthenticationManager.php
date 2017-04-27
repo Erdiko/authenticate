fa5ac1d5d248d9777066efd3d62dd05b79b63be9
Leo Daidone <leodaido@gmail.com>
2017-04-27 19:10:37 -0300

    ER-160
    #5 : added AuthenticationManager, SessionTrait, Updated Unit tests, Updated composer.json

---
+++ src/AuthenticationManager.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * AuthenticationManager
+ *
+ * @package     erdiko/authenticate
+ * @copyright   Copyright (c) 2017, Arroyo Labs, http://www.arroyolabs.com
+ * @author      Leo Daidone, leo@arroyolabs.com
+ */
+
+namespace erdiko\authenticate;
+
+use erdiko\authorize\traits\SessionAccessTrait;
+use Symfony\Component\Security\Core\Authentication\AuthenticationManagerInterface;
+use Symfony\Component\Security\Core\Authentication\AuthenticationProviderManager;
+use Symfony\Component\Security\Core\Authentication\Provider\DaoAuthenticationProvider;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorage;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Encoder\PlaintextPasswordEncoder;
+use Symfony\Component\Security\Core\User\UserChecker;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+
+class AuthenticationManager implements AuthenticationManagerInterface
+{
+    use SessionAccessTrait;
+
+    private $authenticationManager;
+
+    public function __construct(UserProviderInterface $userProvider, $passwordEncoder=null)
+    {
+        $passwordEncoder = empty($passwordEncoder)
+            ? new PlaintextPasswordEncoder()
+            : $passwordEncoder;
+        // Create an encoder factory that will "encode" passwords
+        $encoderFactory = new \Symfony\Component\Security\Core\Encoder\EncoderFactory(array(
+            // We simply use plaintext passwords for users from this specific class
+            'Symfony\Component\Security\Core\User\User' => $passwordEncoder,
+        ));
+
+        // The user checker is a simple class that allows to check against different elements (user disabled, account expired etc)
+        $userChecker = new UserChecker();
+        // The (authentication) providers are a way to make sure to match credentials against users based on their "providerkey".
+        $userProvider = array(
+            new DaoAuthenticationProvider($userProvider, $userChecker, 'main', $encoderFactory, true),
+        );
+
+
+        $this->authenticationManager = new AuthenticationProviderManager($userProvider, true);
+    }
+
+    public function authenticate(TokenInterface $unauthenticatedToken)
+    {
+
+        try {
+            $authenticatedToken = $this->authenticationManager->authenticate($unauthenticatedToken);
+            self::startSession();
+            $tokenStorage = new TokenStorage();
+            $tokenStorage->setToken($authenticatedToken);
+            $_SESSION['tokenstorage'] = $tokenStorage;
+        } catch (\Exception $failed) {
+            // authentication failed
+            throw new \Exception($failed->getMessage());
+        }
+        return $authenticatedToken;
+    }
+}
